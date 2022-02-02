efe6326513a1a8ea59585647dd6001cabe018716
ColtJLaCh <CL51@myscc.ca>
2022-02-02 00:01:11 -0500

    Update colors and add DepthViewTransformer

--- app/src/main/java/coltonlachance/com/madskeletonapplication/VPHolderFragment.java
+++ app/src/main/java/coltonlachance/com/madskeletonapplication/VPHolderFragment.java
@@ -1,9 +1,11 @@
 package coltonlachance.com.madskeletonapplication;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
@@ -71,7 +73,46 @@ public class VPHolderFragment extends Fragment {
         //Set VP2 adapter
         ViewPager2 viewPager2 = view.findViewById(R.id.viewPagerHolder);
         viewPager2.setAdapter(new CustomViewPageAdapter(getActivity()));
-
+        viewPager2.setBackgroundColor(getResources().getColor(R.color.primary));
+        viewPager2.setPageTransformer(new DepthPageTransformer());
         return view;
+    }
+}
+
+class DepthPageTransformer implements ViewPager2.PageTransformer {
+    private static final float MIN_SCALE = 0.75f;
+
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0f);
+
+
+        } else if (position <= 0) { // [-1,0]
+            // Use the default slide transition when moving to the left page
+            view.setAlpha(1f);
+            view.setTranslationX(0f);
+            view.setScaleX(1f);
+            view.setScaleY(1f);
+
+        } else if (position <= 1) { // (0,1]
+            // Fade the page out.
+            view.setAlpha(1 - position);
+
+            // Counteract the default slide transition
+            view.setTranslationX(pageWidth * -position);
+
+            // Scale the page down (between MIN_SCALE and 1)
+            float scaleFactor = MIN_SCALE
+                    + (1 - MIN_SCALE) * (1 - Math.abs(position));
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0f);
+        }
     }
 }
