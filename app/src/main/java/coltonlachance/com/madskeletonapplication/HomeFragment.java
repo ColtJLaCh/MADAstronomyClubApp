22258b072f21abfa09da6f27e638d70227f50415
ColtJLaCh <CL51@myscc.ca>
2022-02-22 20:24:52 -0500

    Make fab button only show on Reycler View fragment

--- app/src/main/java/coltonlachance/com/madskeletonapplication/HomeFragment.java
+++ app/src/main/java/coltonlachance/com/madskeletonapplication/HomeFragment.java
@@ -8,12 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import coltonlachance.com.madskeletonapplication.databinding.ActivityMainBinding;
+
 /**HomeFragment
  * A simple Home fragment used with nav drawer.
  * @author Colton LaChance
  */
 public class HomeFragment extends Fragment {
-
+    
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -47,6 +51,7 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
--- app/src/main/java/coltonlachance/com/madskeletonapplication/MainActivity.java
+++ app/src/main/java/coltonlachance/com/madskeletonapplication/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.Menu;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    NavigationView navigationView;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +37,51 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         setSupportActionBar(binding.appBarMain.toolbar);
 
-
         DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        navigationView = binding.navView;
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_recycler, R.id.nav_vp_holder,R.id.nav_extra)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavDestination currentFragment = navController.getCurrentDestination();
+                if (currentFragment.getId() == R.id.nav_recycler) {
+                    Bundle extra = new Bundle();
+                    /*
+                    extra.putInt(CreateUpdateFragment.ACTION_TYPE,
+                            CreateUpdateFragment.CREATE);
+                    navController
+                            .navigate(R.id.createUpdateFragment, extra);
+                    */
+
+                }
+            }
+        });
+
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller,
+                                             @NonNull NavDestination destination,
+                                             @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.nav_recycler) {
+                    binding.appBarMain.fab.show();
+                } else {
+                    binding.appBarMain.fab.hide();
+                }
+            }
+        });
     }
 
     @Override
